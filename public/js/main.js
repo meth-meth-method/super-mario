67c41cbf4f4ad3814d2c30e673b849b888d64d60
Pontus Persson <pontus.alexander@gmail.com>
2017-09-21 18:29:59 +0200

    Use drawBackground to draw background from JSON.

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,16 @@ import {loadImage, loadLevel} from './loaders.js';
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    });
+}
+
 
 loadImage('/img/tiles.png')
 .then(image => {
@@ -13,17 +23,6 @@ loadImage('/img/tiles.png')
 
     loadLevel('1-1')
     .then(level => {
-        console.log(level);
+        drawBackground(level.backgrounds[0], context, sprites);
     });
-
-    for (let x = 0; x < 30; ++x) {
-        for (let y = 0; y < 15; ++y) {
-            sprites.drawTile('sky', context, x, y);
-        }
-    }
-    for (let x = 0; x < 30; ++x) {
-        for (let y = 13; y < 15; ++y) {
-            sprites.drawTile('ground', context, x, y);
-        }
-    }
 });
