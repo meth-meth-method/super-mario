b65cfd6371a445cc0da45677a42fb709d90de17f
Pontus Persson <pontus.alexander@gmail.com>
2017-09-21 18:01:36 +0200

    Add loadImage function.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
+function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    });
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
