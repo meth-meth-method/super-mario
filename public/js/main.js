1295055a9fbfc986562137aea5a933a18d3b17b0
Pontus Persson <pontus.alexander@gmail.com>
2017-09-21 18:11:30 +0200

    Move SpriteSheet class to module.

--- public/js/main.js
+++ public/js/main.js
@@ -8,39 +8,6 @@ function loadImage(url) {
     });
 }
 
-class SpriteSheet {
-    constructor(image, w = 16, h = 16) {
-        this.image = image;
-        this.width = w;
-        this.height = h;
-        this.tiles = new Map();
-    }
-
-    define(name, x, y) {
-        const buffer = document.createElement('canvas');
-        buffer.height = this.height;
-        buffer.width = this.width;
-        buffer
-            .getContext('2d')
-            .drawImage(
-                this.image,
-                this.width * x,
-                this.height * y,
-                this.width,
-                this.height,
-                0,
-                0,
-                this.width,
-                this.height);
-        this.tiles.set(name, buffer);
-    }
-
-    draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
-        context.drawImage(buffer, x, y);
-    }
-}
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
