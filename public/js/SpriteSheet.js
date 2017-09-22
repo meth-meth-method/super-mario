e5b93e14675abf00a851a8777150cae12d24c268
Pontus Persson <pontus.alexander@gmail.com>
2017-09-22 16:42:18 +0200

    Sync with published result.

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -1,28 +1,28 @@
 export default class SpriteSheet {
-    constructor(image, w = 16, h = 16) {
+    constructor(image, width, height) {
         this.image = image;
-        this.width = w;
-        this.height = h;
+        this.width = width;
+        this.height = height;
         this.tiles = new Map();
     }
 
     define(name, x, y) {
         const buffer = document.createElement('canvas');
-        buffer.height = this.height;
         buffer.width = this.width;
+        buffer.height = this.height;
         buffer
             .getContext('2d')
             .drawImage(
                 this.image,
-                this.width * x,
-                this.height * y,
+                x * this.width,
+                y * this.height,
                 this.width,
                 this.height,
                 0,
                 0,
                 this.width,
                 this.height);
-        this.tiles.set(name, buffer);
+        this.tiles.set(name, buffer)
     }
 
     draw(name, context, x, y) {
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
 import SpriteSheet from './SpriteSheet.js';
 import {loadImage, loadLevel} from './loaders.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([x1, x2, y1, y2]) => {
         for (let x = x1; x < x2; ++x) {
@@ -14,17 +11,19 @@ function drawBackground(background, context, sprites) {
     });
 }
 
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
 
 loadImage('/img/tiles.png')
 .then(image => {
-    const sprites = new SpriteSheet(image);
+    const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
 
     loadLevel('1-1')
     .then(level => {
-        level.backgrounds.forEach(bg => {
-            drawBackground(bg, context, sprites);
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, sprites);
         });
     });
 });
