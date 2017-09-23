33ba18b85f7c00efb20583987097ac1f6e45c929
Pontus Persson <pontus.alexander@gmail.com>
2017-09-23 18:16:13 +0200

    Decouple background drawing to layers module.

---
+++ public/js/layers.js
@@ -0,0 +1,23 @@
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    });
+}
+
+export function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 240;
+
+    backgrounds.forEach(background => {
+        drawBackground(background, buffer.getContext('2d'), sprites);
+    });
+
+    return function drawBackgroundLayer(context) {
+        context.drawImage(buffer, 0, 0);
+    };
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,11 @@
 import Compositor from './Compositor.js';
 import {loadLevel} from './loaders.js';
 import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
-
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
+import {createBackgroundLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createBackgroundLayer(backgrounds, sprites) {
-    const buffer = document.createElement('canvas');
-    buffer.width = 256;
-    buffer.height = 240;
-
-    backgrounds.forEach(background => {
-        drawBackground(background, buffer.getContext('2d'), sprites);
-    });
-
-    return function drawBackgroundLayer(context) {
-        context.drawImage(buffer, 0, 0);
-    };
-}
 
 function createSpriteLayer(sprite, pos) {
     return function drawSpriteLayer(context) {
