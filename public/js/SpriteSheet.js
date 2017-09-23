b728080a7452f78a187774d563c4dae7e4a62fb1
Pontus Persson <pontus.alexander@gmail.com>
2017-09-23 10:53:55 +0200

    Refactor SpriteSheet to allow free tile define.

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -6,23 +6,32 @@ export default class SpriteSheet {
         this.tiles = new Map();
     }
 
-    define(name, x, y) {
+    define(name, x, y, width, height) {
         const buffer = document.createElement('canvas');
-        buffer.width = this.width;
-        buffer.height = this.height;
+        buffer.width = width;
+        buffer.height = height;
         buffer
             .getContext('2d')
             .drawImage(
                 this.image,
-                x * this.width,
-                y * this.height,
-                this.width,
-                this.height,
+                x,
+                y,
+                width,
+                height,
                 0,
                 0,
-                this.width,
-                this.height);
+                width,
+                height);
         this.tiles.set(name, buffer)
+    }
+
+    defineTile(name, x, y) {
+        this.define(
+            name,
+            x * this.width,
+            y * this.height,
+            this.width,
+            this.height);
     }
 
     draw(name, context, x, y) {
