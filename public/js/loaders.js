a2f9f8fe14bedc289b8747c29a908f6ba808f7fc
Pontus Persson <pontus.alexander@gmail.com>
2017-09-23 11:29:28 +0200

    Remove artificial lag.

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -2,7 +2,7 @@ export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
         image.addEventListener('load', () => {
-            setTimeout(resolve, 2000, image);
+            resolve(image);
         });
         image.src = url;
     });
@@ -10,6 +10,5 @@ export function loadImage(url) {
 
 export function loadLevel(name) {
     return fetch(`/levels/${name}.json`)
-    .then(r => r.json())
-    .then(json => new Promise(resolve => setTimeout(resolve, 3000, json)));
+    .then(r => r.json());
 }
