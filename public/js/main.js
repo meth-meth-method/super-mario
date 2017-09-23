023586e2ab0a9a7994215fc788e13489e8e9d15f
Pontus Persson <pontus.alexander@gmail.com>
2017-09-23 11:23:31 +0200

    Add log message when resource loaded.

--- public/js/main.js
+++ public/js/main.js
@@ -16,12 +16,14 @@ const context = canvas.getContext('2d');
 
 loadImage('/img/tiles.png')
 .then(image => {
+    console.log('Image loaded', image);
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
 
     loadLevel('1-1')
     .then(level => {
+        console.log('Level loader', level);
         level.backgrounds.forEach(background => {
             drawBackground(background, context, sprites);
         });
