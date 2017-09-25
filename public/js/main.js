4db2dc254ba3c1d0ecfeb46c70f8f94a6b8fa9ac
Pontus Persson <pontus.alexander@gmail.com>
2017-09-25 20:36:20 +0200

    Add Timer class.

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,35 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 
+class Timer {
+    constructor(deltaTime) {
+        let accumulatedTime = 0;
+        let lastTime = 0;
+
+        this.updateProxy = (time) => {
+            accumulatedTime += (time - lastTime) / 1000;
+
+            while (accumulatedTime > deltaTime) {
+                this.update(deltaTime);
+                accumulatedTime -= deltaTime;
+            }
+
+            lastTime = time;
+
+            this.enqueue();
+        };
+    }
+
+    enqueue() {
+        requestAnimationFrame(this.updateProxy);
+    }
+
+    start() {
+        this.enqueue();
+    }
+}
+
+
 Promise.all([
     createMario(),
     loadBackgroundSprites(),
@@ -30,27 +59,14 @@ Promise.all([
     const spriteLayer = createSpriteLayer(entities);
     comp.layers.push(spriteLayer);
 
-    const deltaTime = 1/60;
-    let accumulatedTime = 0;
-    let lastTime = 0;
-    function update(time) {
-        accumulatedTime += (time - lastTime) / 1000;
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
+        entities.forEach(entity => {
+            entity.update(deltaTime);
+            entity.vel.y += 30;
+        });
+        comp.draw(context);
+    };
 
-        while (accumulatedTime > deltaTime) {
-            entities.forEach(entity => {
-                entity.update(deltaTime);
-                entity.vel.y += 30;
-            });
-
-            comp.draw(context);
-
-            accumulatedTime -= deltaTime;
-        }
-
-        requestAnimationFrame(update);
-
-        lastTime = time;
-    }
-
-    update(0);
+    timer.start();
 });
