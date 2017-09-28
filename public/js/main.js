3d32b1abee73493967abf8790113b093804a953b
Pontus Persson <pontus.alexander@gmail.com>
2017-09-28 23:18:24 +0200

    Refactor to support Level class.

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,22 @@
-import Compositor from './Compositor.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
     createMario(),
-    loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-.then(([mario, backgroundSprites, level]) => {
-    const comp = new Compositor();
-
-    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-    comp.layers.push(backgroundLayer);
-
+.then(([mario, level]) => {
     const gravity = 30;
     mario.pos.set(64, 180);
     mario.vel.set(200, -600);
 
-
-    const spriteLayer = createSpriteLayer(mario);
-    comp.layers.push(spriteLayer);
-
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
-        comp.draw(context);
+        level.comp.draw(context);
         mario.update(deltaTime);
         mario.vel.y += gravity;
     }
