11cf7702a79b59d44c12a33727c17c5cba859245
Pontus Persson <pontus.alexander@gmail.com>
2017-09-28 23:33:03 +0200

    Remove unused draw function.

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,13 +1,3 @@
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
-
 export function createBackgroundLayer(tiles, sprites) {
     const buffer = document.createElement('canvas');
     buffer.width = 256;
