d82baf3ac4e47c169dec13dfcf1a96bd3d07fb7c
Pontus Persson <pontus.alexander@gmail.com>
2017-09-28 23:29:33 +0200

    Populate tile Matrix from JSON.

--- public/js/loaders/level.js
+++ public/js/loaders/level.js
@@ -2,6 +2,20 @@ import Level from '../Level.js';
 import {loadBackgroundSprites} from '../sprites.js';
 import {createBackgroundLayer, createSpriteLayer} from '../layers.js';
 
+function createTiles(level, backgrounds) {
+    backgrounds.forEach(background => {
+        background.ranges.forEach(([x1, x2, y1, y2]) => {
+            for (let x = x1; x < x2; ++x) {
+                for (let y = y1; y < y2; ++y) {
+                    level.tiles.set(x, y, {
+                        graphic: background.tile,
+                    });
+                }
+            }
+        });
+    });
+}
+
 export function loadLevel(name) {
     return Promise.all([
         fetch(`/levels/${name}.json`)
@@ -11,6 +25,8 @@ export function loadLevel(name) {
     ])
     .then(([levelSpec, backgroundSprites]) => {
         const level = new Level();
+
+        createTiles(level, levelSpec.backgrounds);
 
         const backgroundLayer = createBackgroundLayer(levelSpec.backgrounds, backgroundSprites);
         level.comp.layers.push(backgroundLayer);
