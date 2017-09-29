c2a873f0fe4902076c8efea386a04f6036735b61
Pontus Persson <pontus.alexander@gmail.com>
2017-09-29 10:56:05 +0200

    Use Matrix.forEach to draw.

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,13 +1,13 @@
+import {Matrix} from './math.js';
+
 export function createBackgroundLayer(tiles, sprites) {
     const buffer = document.createElement('canvas');
     buffer.width = 256;
     buffer.height = 240;
 
     const context = buffer.getContext('2d');
-    tiles.grid.forEach((column, x) => {
-        column.forEach((tile, y) => {
-            sprites.drawTile(tile.graphic, context, x, y);
-        });
+    tiles.forEach((tile, x, y) => {
+        sprites.drawTile(tile.graphic, context, x, y);
     });
 
     return function drawBackgroundLayer(context) {
