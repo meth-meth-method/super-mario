a16d0af571df73e3ab650d66588e391575ab31b3
Pontus Persson <pontus.alexander@gmail.com>
2017-09-29 10:52:52 +0200

    Add size to entity.

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -1,8 +1,10 @@
+import BoundingBox from './BoundingBox.js';
 import {Vec2} from './math.js';
 
 export default class Entity {
     constructor() {
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
+        this.size = new Vec2(0, 0);
     }
 }
