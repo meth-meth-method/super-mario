fc507507eb91ae9d0999ee870f1849199ab6aede
Pontus Persson <pontus.alexander@gmail.com>
2017-10-01 20:54:00 +0200

    Add Matrix data structure and use as tiles property in Level.

--- public/js/math.js
+++ public/js/math.js
@@ -1,3 +1,25 @@
+export class Matrix {
+    constructor() {
+        this.grid = [];
+    }
+
+    get(x, y) {
+        const col = this.grid[x];
+        if (col) {
+            return col[y];
+        }
+        return undefined;
+    }
+
+    set(x, y, value) {
+        if (!this.grid[x]) {
+            this.grid[x] = [];
+        }
+
+        this.grid[x][y] = value;
+    }
+}
+
 export class Vec2 {
     constructor(x, y) {
         this.set(x, y);
