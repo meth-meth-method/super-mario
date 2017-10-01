1396780afe6c59ef4444adbb724a695cf1714a45
Pontus Persson <pontus.alexander@gmail.com>
2017-10-01 14:53:34 +0200

    Add Trait support to Entity class.

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -4,5 +4,18 @@ export default class Entity {
     constructor() {
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
+
+        this.traits = [];
+    }
+
+    addTrait(trait) {
+        this.traits.push(trait);
+        this[trait.NAME] = trait;
+    }
+
+    update(deltaTime, input) {
+        this.traits.forEach(trait => {
+            trait.update(this, deltaTime);
+        });
     }
 }
