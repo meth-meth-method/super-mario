40a193ec945b32116c17654ca403d1e8e0ca9f61
Pontus Persson <pontus.alexander@gmail.com>
2017-10-01 20:54:00 +0200

    Add Matrix data structure and use as tiles property in Level.

--- public/js/Level.js
+++ public/js/Level.js
@@ -1,8 +1,10 @@
 import Compositor from './Compositor.js';
+import {Matrix} from './math.js';
 
 export default class Level {
     constructor() {
         this.comp = new Compositor();
         this.entities = new Set();
+        this.tiles = new Matrix();
     }
 }
