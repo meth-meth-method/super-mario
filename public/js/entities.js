58fae4587b91a81b283d3c43a26fff0533861e0f
Pontus Persson <pontus.alexander@gmail.com>
2017-10-01 11:26:58 +0200

    Remove position update from Mario.

--- public/js/entities.js
+++ public/js/entities.js
@@ -12,8 +12,6 @@ export function createMario() {
         }
 
         mario.update = function updateMario(deltaTime) {
-            this.pos.x += this.vel.x * deltaTime;
-            this.pos.y += this.vel.y * deltaTime;
         }
 
         return mario;
