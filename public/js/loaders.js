622ac12aadf6a426c13f6789f6ac60e4dad0f740
Pontus Persson <pontus.alexander@gmail.com>
2017-10-01 20:44:30 +0200

    Handle Level class output from loadLevel in main.

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,7 @@
+import Level from './Level.js';
+import {loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+
 export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
@@ -9,6 +13,21 @@ export function loadImage(url) {
 }
 
 export function loadLevel(name) {
-    return fetch(`/levels/${name}.json`)
-    .then(r => r.json());
+    return Promise.all([
+        fetch(`/levels/${name}.json`)
+        .then(r => r.json()),
+
+        loadBackgroundSprites(),
+    ])
+    .then(([levelSpec, backgroundSprites]) => {
+        const level = new Level();
+
+        const backgroundLayer = createBackgroundLayer(levelSpec.backgrounds, backgroundSprites);
+        level.comp.layers.push(backgroundLayer);
+
+        const spriteLayer = createSpriteLayer(level.entities);
+        level.comp.layers.push(spriteLayer);
+
+        return level;
+    });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
-import Compositor from './Compositor.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
 import Keyboard from './KeyboardState.js';
 
@@ -13,15 +10,9 @@ const context = canvas.getContext('2d');
 
 Promise.all([
     createMario(),
-    loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-.then(([mario, backgroundSprites, level]) => {
-    const comp = new Compositor();
-
-    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-    comp.layers.push(backgroundLayer);
-
+.then(([mario, level]) => {
     const gravity = 2000;
     mario.pos.set(64, 180);
 
@@ -37,15 +28,11 @@ Promise.all([
     });
     input.listenTo(window);
 
-
-    const spriteLayer = createSpriteLayer(mario);
-    comp.layers.push(spriteLayer);
-
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
         mario.update(deltaTime);
 
-        comp.draw(context);
+        level.comp.draw(context);
 
         mario.vel.y += gravity * deltaTime;
     }
