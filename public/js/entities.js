63c666c3912360507f63c9df2a261f4f1d77002c
Pontus Persson <pontus.alexander@gmail.com>
2017-10-02 21:20:22 +0200

    Apply Go trait to Mario.

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,6 +1,7 @@
 import Entity from './Entity.js';
 import Jump from './traits/Jump.js';
 import Velocity from './traits/Velocity.js';
+import Go from './traits/Go.js';
 import {loadMarioSprite} from './sprites.js';
 
 export function createMario() {
@@ -9,8 +10,9 @@ export function createMario() {
         const mario = new Entity();
         mario.size.set(14, 16);
 
-        mario.addTrait(new Jump());
         mario.addTrait(new Velocity());
+        mario.addTrait(new Jump());
+        mario.addTrait(new Go());
 
         mario.draw = function drawMario(context) {
             sprite.draw('idle', context, this.pos.x, this.pos.y);
