3f2517b60c08584f16d3f83d54116ad72068aaa0
Pontus Persson <pontus.alexander@gmail.com>
2017-10-02 17:46:42 +0200

    Draw collision layer.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import Timer from './Timer.js';
 import {loadLevel} from './loaders/level.js';
 import {createMario} from './entities.js';
 import {setupInput} from './keyboard.js';
+import {createCollisionLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -18,6 +19,8 @@ Promise.all([
     setupInput(mario);
 
     level.entities.add(mario);
+
+    level.comp.layers.push(createCollisionLayer(level));
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
