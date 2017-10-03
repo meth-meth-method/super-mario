9abdc1bb65af99c559a61bf78d55b6fa371352b4
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 15:53:48 +0200

    Refactor Keyboard code to remove deprecated keyCode prop.

--- public/js/input.js
+++ public/js/input.js
@@ -1,19 +1,18 @@
 import Keyboard from './KeyboardState.js';
 
 export function setupKeyboard(entity) {
-    const SPACE = 32;
     const input = new Keyboard();
-    input.addMapping(SPACE, keyState => {
+    input.addMapping('Space', keyState => {
         if (keyState) {
             entity.jump.start();
         } else {
             entity.jump.cancel();
         }
     });
-    input.addMapping(39, keyState => {
+    input.addMapping('ArrowRight', keyState => {
         entity.go.dir = keyState;
     });
-    input.addMapping(37, keyState => {
+    input.addMapping('ArrowLeft', keyState => {
         entity.go.dir = -keyState;
     });
     return input;
