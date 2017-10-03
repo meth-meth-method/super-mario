245d018d695a5a211c76564da55d39a34126c3aa
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 16:03:35 +0200

    Insert several Mario.

--- public/js/main.js
+++ public/js/main.js
@@ -8,18 +8,19 @@ const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-    createMario(),
+    Promise.all(new Array(100).fill(0).map(() => createMario())),
     loadLevel('1-1'),
 ])
-.then(([mario, level]) => {
-    mario.pos.set(64, 64);
+.then(([marios, level]) => {
+    marios.forEach(mario => {
+        const input = setupKeyboard(mario);
+        input.listenTo(window);
 
-    level.entities.add(mario);
+        mario.pos.set(200 * Math.random(), 64);
+        level.entities.add(mario);
+    });
 
     level.comp.layers.push(createCollisionLayer(level));
-
-    const input = setupKeyboard(mario);
-    input.listenTo(window);
 
     ['mousedown', 'mousemove'].forEach(eventName => {
         canvas.addEventListener(eventName, event => {
