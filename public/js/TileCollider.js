c82233ea2db74a6815bc5f73d63db2759ce7dd43
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 13:46:39 +0200

    Add TileResolver class and find tile by entity position.

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -1,9 +1,40 @@
+class TileResolver {
+    constructor(matrix, tileSize = 16) {
+        this.tileSize = tileSize;
+        this.matrix = matrix;
+    }
+
+    toIndex(pos) {
+        return Math.floor(pos / this.tileSize);
+    }
+
+    getByIndex(indexX, indexY) {
+        const tile = this.matrix.get(indexX, indexY);
+        if (tile) {
+            return {
+                tile,
+                indexX,
+                indexY,
+            };
+        }
+    }
+
+    matchByPosition(posX, posY) {
+        return this.getByIndex(
+            this.toIndex(posX),
+            this.toIndex(posY));
+    }
+}
+
 export default class TileCollider {
-    constructor(tiles) {
-        this.tiles = tiles;
+    constructor(tileMatrix) {
+        this.tiles = new TileResolver(tileMatrix);
     }
 
     test(entity) {
-        console.log('Testing', entity);
+        const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
+        if (match) {
+            console.log('Matching tile', match, match.tile);
+        }
     }
 }
