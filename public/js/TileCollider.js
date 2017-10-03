aea2b073b14edce96ef9ee528d620487dbbfa78c
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 14:18:40 +0200

    Implement basic ground collision detection.

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -5,10 +5,25 @@ export default class TileCollider {
         this.tiles = new TileResolver(tileMatrix);
     }
 
-    test(entity) {
+    checkY(entity) {
         const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
-        if (match) {
-            console.log('Matching tile', match, match.tile);
+        if (!match) {
+            return;
         }
+
+        if (match.tile.name !== 'ground') {
+            return;
+        }
+
+        if (entity.vel.y > 0) {
+            if (entity.pos.y > match.y1) {
+                entity.pos.y = match.y1;
+                entity.vel.y = 0;
+            }
+        }
+    }
+
+    test(entity) {
+        this.checkY(entity);
     }
 }
--- public/js/TileResolver.js
+++ public/js/TileResolver.js
@@ -11,10 +11,12 @@ export default class TileResolver {
     getByIndex(indexX, indexY) {
         const tile = this.matrix.get(indexX, indexY);
         if (tile) {
+            const y1 = indexY * this.tileSize;
             return {
                 tile,
                 indexX,
                 indexY,
+                y1,
             };
         }
     }
