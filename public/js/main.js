869c959a48a3fc2612acaf142da859b1b86ca56a
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 14:20:58 +0200

    Add debug purpose mouse controls for Mario sprite.

--- public/js/main.js
+++ public/js/main.js
@@ -31,6 +31,14 @@ Promise.all([
     });
     input.listenTo(window);
 
+    ['mousedown', 'mousemove'].forEach(eventName => {
+        canvas.addEventListener(eventName, event => {
+            if (event.buttons === 1) {
+                mario.vel.set(0, 0);
+                mario.pos.set(event.offsetX, event.offsetY);
+            }
+        });
+    });
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
