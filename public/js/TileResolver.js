be0f289f1a391c26ecc2d9e2844cac584a452b60
Pontus Persson <pontus.alexander@gmail.com>
2017-10-03 15:07:55 +0200

    Add range methods to TileResolver.

--- public/js/TileResolver.js
+++ public/js/TileResolver.js
@@ -8,15 +8,30 @@ export default class TileResolver {
         return Math.floor(pos / this.tileSize);
     }
 
+    toIndexRange(pos1, pos2) {
+        const pMax = Math.ceil(pos2 / this.tileSize) * this.tileSize;
+        const range = [];
+        let pos = pos1;
+        do {
+            range.push(this.toIndex(pos));
+            pos = pos + this.tileSize;
+        } while (pos < pMax);
+        return range;
+    }
+
     getByIndex(indexX, indexY) {
         const tile = this.matrix.get(indexX, indexY);
         if (tile) {
+            const x1 = indexX * this.tileSize;
+            const x2 = x1 + this.tileSize;
             const y1 = indexY * this.tileSize;
             const y2 = y1 + this.tileSize;
             return {
                 tile,
                 indexX,
                 indexY,
+                x1,
+                x2,
                 y1,
                 y2,
             };
@@ -27,5 +42,18 @@ export default class TileResolver {
         return this.getByIndex(
             this.toIndex(posX),
             this.toIndex(posY));
+    }
+
+    matchByRange(x1, x2, y1, y2) {
+        const matches = [];
+        this.toIndexRange(x1, x2).forEach(indexX => {
+            this.toIndexRange(y1, y2).forEach(indexY => {
+                const match = this.getByIndex(indexX, indexY);
+                if (match) {
+                    matches.push(match);
+                }
+            });
+        });
+        return matches;
     }
 }
