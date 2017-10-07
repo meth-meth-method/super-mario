6462fd729f8e750e3471dfba0ab6e1d681968899
Pontus Persson <pontus.alexander@gmail.com>
2017-10-07 14:59:33 +0200

    Update Sprite Layer drawing to consider camera position.

--- public/js/entities.js
+++ public/js/entities.js
@@ -15,7 +15,7 @@ export function createMario() {
         //mario.addTrait(new Velocity());
 
         mario.draw = function drawMario(context) {
-            sprite.draw('idle', context, this.pos.x, this.pos.y);
+            sprite.draw('idle', context, 0, 0);
         }
 
         return mario;
--- public/js/layers.js
+++ public/js/layers.js
@@ -14,10 +14,24 @@ export function createBackgroundLayer(level, sprites) {
     };
 }
 
-export function createSpriteLayer(entities) {
-    return function drawSpriteLayer(context) {
+export function createSpriteLayer(entities, maxWidth = 64, maxHeight = 64) {
+    const spriteBuffer = document.createElement('canvas');
+    spriteBuffer.width = maxWidth;
+    spriteBuffer.height = maxHeight;
+    const spriteBufferContext = spriteBuffer.getContext('2d');
+
+    return function drawSpriteLayer(context, camera) {
+        const {pos} = camera;
+
         entities.forEach(entity => {
-            entity.draw(context);
+            spriteBufferContext.clearRect(0, 0, maxWidth, maxHeight);
+
+            entity.draw(spriteBufferContext);
+
+            context.drawImage(
+                spriteBuffer,
+                entity.pos.x - pos.x,
+                entity.pos.y - pos.y);
         });
     };
 }
