620b13dee34b6ac404ae7ca87d1fb2a5e83cf1b2
Pontus Persson <pontus.alexander@gmail.com>
2017-10-07 15:11:14 +0200

    Add camera control to mouse button 2.

--- public/js/debug.js
+++ public/js/debug.js
@@ -1,4 +1,6 @@
 export function setupMouseControl(canvas, entity, camera) {
+    let lastEvent;
+
     ['mousedown', 'mousemove'].forEach(eventName => {
         canvas.addEventListener(eventName, event => {
             if (event.buttons === 1) {
@@ -6,7 +8,16 @@ export function setupMouseControl(canvas, entity, camera) {
                 entity.pos.set(
                     event.offsetX + camera.pos.x,
                     event.offsetY + camera.pos.y);
+            } else if (event.buttons === 2
+                && lastEvent && lastEvent.buttons === 2
+                && lastEvent.type === 'mousemove') {
+                camera.pos.x -= event.offsetX - lastEvent.offsetX;
             }
+            lastEvent = event;
         });
+    });
+
+    canvas.addEventListener('contextmenu', event => {
+        event.preventDefault();
     });
 }
