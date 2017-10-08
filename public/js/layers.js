fa94fa32144263efa12abcb9ab66eacf61732ba2
Pontus Persson <pontus.alexander@gmail.com>
2017-10-08 13:49:02 +0200

    Create camera layer.

--- public/js/layers.js
+++ public/js/layers.js
@@ -73,3 +73,16 @@ export function createCollisionLayer(level) {
         resolvedTiles.length = 0;
     };
 }
+
+export function createCameraLayer(cameraToDraw) {
+    return function drawCameraRect(context, fromCamera) {
+        context.strokeStyle = 'purple';
+        context.beginPath();
+        context.rect(
+            cameraToDraw.pos.x - fromCamera.pos.x,
+            cameraToDraw.pos.y - fromCamera.pos.y,
+            cameraToDraw.size.x,
+            cameraToDraw.size.y);
+        context.stroke();
+    };
+}
