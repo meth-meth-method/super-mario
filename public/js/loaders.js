69a45695c4b3e8aec572233ae585b84d8d3d3fb2
Pontus Persson <pontus.alexander@gmail.com>
2017-10-08 10:50:38 +0200

    Refactor createTile function to allow multiple definitions of ranges and.

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -40,17 +40,36 @@ function loadSpriteSheet(name) {
 }
 
 function createTiles(level, backgrounds) {
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
+        for (let x = xStart; x < xEnd; ++x) {
+            for (let y = yStart; y < yEnd; ++y) {
+                level.tiles.set(x, y, {
+                    name: background.sprite,
+                    type: background.type,
+                });
+            }
+        }
+    }
+
     backgrounds.forEach(background => {
-        background.ranges.forEach(([xStart, xLen, yStart, yLen]) => {
-            const xEnd = xStart + xLen;
-            const yEnd = yStart + yLen;
-            for (let x = xStart; x < xEnd; ++x) {
-                for (let y = yStart; y < yEnd; ++y) {
-                    level.tiles.set(x, y, {
-                        name: background.sprite,
-                        type: background.type,
-                    });
-                }
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen);
+
+            } else if (range.length === 3) {
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1);
+
+            } else if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1);
+
+            } else {
+                console.error(range);
+                throw new TypeError('Illegal range definition.');
             }
         });
     });
