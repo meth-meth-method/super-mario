f56f7cbd359864267a17fcfe30aa1c3663ced600
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 01:05:36 +0200

    Add dampening effect on running.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -6,6 +6,7 @@ export default class Go extends Trait {
 
         this.dir = 0;
         this.acceleration = 400;
+        this.deceleration = 300;
         this.dragFactor = 1/5000;
 
         this.distance = 0;
@@ -15,10 +16,13 @@ export default class Go extends Trait {
     update(entity, deltaTime) {
         const absX = Math.abs(entity.vel.x);
 
-        if (this.dir) {
+        if (this.dir !== 0) {
             entity.vel.x += this.acceleration * deltaTime * this.dir;
             this.heading = this.dir;
             this.distance += absX * deltaTime;
+        } else if (entity.vel.x !== 0) {
+            const decel = Math.min(absX, this.deceleration * deltaTime);
+            entity.vel.x += entity.vel.x > 0 ? -decel : decel;
         } else {
             this.distance = 0;
         }
