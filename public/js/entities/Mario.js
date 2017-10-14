2fa181eb811898042e07dbee3db365ff733a3cb9
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 13:30:36 +0200

    Add offset to Mario and level detail for testing purposes.

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -45,6 +45,7 @@ function createMarioFactory(sprite) {
     return function createMario() {
         const mario = new Entity();
         mario.size.set(14, 16);
+        mario.offset.set(8, 12);
 
         mario.addTrait(new Go());
         mario.addTrait(new Jump());
