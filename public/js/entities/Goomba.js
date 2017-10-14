f1a15be80fa2298d83c97831feabc7d213639ce1
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 11:52:51 +0200

    Add Goomba entity.

---
+++ public/js/entities/Goomba.js
@@ -0,0 +1,22 @@
+import Entity from '../Entity.js';
+import {loadSpriteSheet} from '../loaders.js';
+
+export function loadGoomba() {
+    return loadSpriteSheet('goomba')
+    .then(createGoombaFactory);
+}
+
+function createGoombaFactory(sprite) {
+    function drawGoomba(context) {
+        sprite.draw('walk-1', context, 0, 0);
+    }
+
+    return function createGoomba() {
+        const goomba = new Entity();
+        goomba.size.set(16, 16);
+
+        goomba.draw = drawGoomba;
+
+        return goomba;
+    };
+}
--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import Camera from './Camera.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders/level.js';
 import {loadMario} from './entities/Mario.js';
+import {loadGoomba} from './entities/Goomba.js';
 import {setupKeyboard} from './input.js';
 
 const canvas = document.getElementById('screen');
@@ -9,9 +10,10 @@ const context = canvas.getContext('2d');
 
 Promise.all([
     loadMario(),
+    loadGoomba(),
     loadLevel('1-1'),
 ])
-.then(([createMario, level]) => {
+.then(([createMario, createGoomba, level]) => {
     const camera = new Camera();
 
     function spawnMario(posX, posY) {
@@ -28,6 +30,10 @@ Promise.all([
     canvas.addEventListener('click', event => {
         spawnMario(event.offsetX + camera.pos.x, event.offsetY);
     });
+
+    const goomba = createGoomba();
+    goomba.pos.x = 220;
+    level.entities.add(goomba);
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
