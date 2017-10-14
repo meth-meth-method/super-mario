412246cc81fb2b75ac7ae93235a60e396dfce4d3
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 09:38:24 +0200

    Create a flipped version of tiles.

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -7,12 +7,18 @@ export default class SpriteSheet {
     }
 
     define(name, x, y, width, height) {
-        const buffer = document.createElement('canvas');
-        buffer.width = width;
-        buffer.height = height;
-        buffer
-            .getContext('2d')
-            .drawImage(
+        const buffers = [false, true].map(flip => {
+            const buffer = document.createElement('canvas');
+            buffer.width = width;
+            buffer.height = height;
+
+            const context = buffer.getContext('2d');
+            if (flip) {
+                context.scale(-1, 1);
+                context.translate(-width, 0);
+            }
+
+            context.drawImage(
                 this.image,
                 x,
                 y,
@@ -22,15 +28,19 @@ export default class SpriteSheet {
                 0,
                 width,
                 height);
-        this.tiles.set(name, buffer)
+
+            return buffer;
+        });
+
+        this.tiles.set(name, buffers);
     }
 
     defineTile(name, x, y) {
         this.define(name, x * this.width, y * this.height, this.width, this.height);
     }
 
-    draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
+    draw(name, context, x, y, flip = false) {
+        const buffer = this.tiles.get(name)[flip ? 1 : 0];
         context.drawImage(buffer, x, y);
     }
 
