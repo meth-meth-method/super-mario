ad678c255a2c52a2bbb96895abfe082258b3b63a
Pontus Persson <pontus.alexander@gmail.com>
2017-10-15 15:57:37 +0200

    Add distance calculation to Go trait.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -6,9 +6,13 @@ export default class Go extends Trait {
 
         this.dir = 0;
         this.speed = 6000;
+
+        this.distance = 0;
     }
 
     update(entity, deltaTime) {
         entity.vel.x = this.speed * this.dir * deltaTime;
+
+        this.distance += entity.vel.x * deltaTime;
     }
 }
