78e92c4d068ba94922bfd0a2213e4d0dddaacd92
Pontus Persson <pontus.alexander@gmail.com>
2017-10-15 09:37:39 +0200

    Add totalTime to Level and draw animations if defined in backgroundLayer.

--- public/js/Level.js
+++ public/js/Level.js
@@ -5,6 +5,7 @@ import {Matrix} from './math.js';
 export default class Level {
     constructor() {
         this.gravity = 2000;
+        this.totalTime = 0;
 
         this.comp = new Compositor();
         this.entities = new Set();
@@ -25,5 +26,7 @@ export default class Level {
 
             entity.vel.y += this.gravity * deltaTime;
         });
+
+        this.totalTime += deltaTime;
     }
 }
--- public/js/layers.js
+++ public/js/layers.js
@@ -21,7 +21,11 @@ export function createBackgroundLayer(level, sprites) {
             const col = tiles.grid[x];
             if (col) {
                 col.forEach((tile, y) => {
-                    sprites.drawTile(tile.name, context, x - startIndex, y);
+                    if (sprites.animations.has(tile.name)) {
+                        sprites.drawAnim(tile.name, context, x - startIndex, y, level.totalTime);
+                    } else {
+                        sprites.drawTile(tile.name, context, x - startIndex, y);
+                    }
                 });
             }
         }
