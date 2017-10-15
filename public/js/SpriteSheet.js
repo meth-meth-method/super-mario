1be9b81c999f5dd2d3141aeeb57c4e4d64dcb7b5
Pontus Persson <pontus.alexander@gmail.com>
2017-10-15 09:36:47 +0200

    Add animation support to SpriteSheet.

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -3,7 +3,12 @@ export default class SpriteSheet {
         this.image = image;
         this.width = width;
         this.height = height;
+        this.animations = new Map();
         this.tiles = new Map();
+    }
+
+    defineAnim(name, animation) {
+        this.animations.set(name, animation);
     }
 
     define(name, x, y, width, height) {
@@ -42,6 +47,11 @@ export default class SpriteSheet {
     draw(name, context, x, y, flip = false) {
         const buffer = this.tiles.get(name)[flip ? 1 : 0];
         context.drawImage(buffer, x, y);
+    }
+
+    drawAnim(name, context, x, y, distance) {
+        const animation = this.animations.get(name);
+        this.drawTile(animation(distance), context, x, y);
     }
 
     drawTile(name, context, x, y) {
