88aa8641be6debcbc73d4b268d6eed82aac40cda
Pontus Persson <pontus.alexander@gmail.com>
2017-10-16 20:43:33 +0200

    Add Jump frame and route for Jump state.

--- public/js/entities.js
+++ public/js/entities.js
@@ -15,6 +15,10 @@ export function createMario() {
 
         const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 10);
         function routeFrame(mario) {
+            if (!mario.jump.ready) {
+                return 'jump';
+            }
+
             if (mario.go.distance > 0) {
                 if (
                     (mario.vel.x > 0 && mario.go.dir < 0) ||
