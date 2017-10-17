519bef52b42106cc977ae27f1fed4fea8ed501c3
Pontus Persson <pontus.alexander@gmail.com>
2017-10-17 13:06:08 +0200

    Remove unused layers and mouse control.

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,7 @@ import Camera from './Camera.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {setupKeyboard} from './input.js';
-import {setupMouseControl} from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -19,18 +17,11 @@ Promise.all([
 
     mario.pos.set(64, 64);
 
-    /*level.comp.layers.push(
-        createCollisionLayer(level),
-        createCameraLayer(camera));*/
-
 
     level.entities.add(mario);
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
-
-    setupMouseControl(canvas, mario, camera);
-
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
