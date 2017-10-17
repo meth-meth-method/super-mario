757d5983785b3db15c20902b175567e1a07aa657
Pontus Persson <pontus.alexander@gmail.com>
2017-10-17 13:05:42 +0200

    Rename entity variable in setupKeyboard to mario.

--- public/js/input.js
+++ public/js/input.js
@@ -1,22 +1,22 @@
 import Keyboard from './KeyboardState.js';
 
-export function setupKeyboard(entity) {
+export function setupKeyboard(mario) {
     const input = new Keyboard();
 
     input.addMapping('KeyP', keyState => {
         if (keyState) {
-            entity.jump.start();
+            mario.jump.start();
         } else {
-            entity.jump.cancel();
+            mario.jump.cancel();
         }
     });
 
     input.addMapping('KeyD', keyState => {
-        entity.go.dir += keyState ? 1 : -1;
+        mario.go.dir += keyState ? 1 : -1;
     });
 
     input.addMapping('KeyA', keyState => {
-        entity.go.dir += keyState ? -1 : 1;
+        mario.go.dir += keyState ? -1 : 1;
     });
 
     return input;
