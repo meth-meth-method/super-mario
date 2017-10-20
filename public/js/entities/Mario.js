5b98abebabac0213c1704afdb3def871c3a17ed4
Pontus Persson <pontus.alexander@gmail.com>
2017-10-20 09:17:06 +0200

    Move routeFrame back into Factory scope to leverage anim parsing.

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -7,26 +7,6 @@ import {createAnim} from '../anim.js';
 const SLOW_DRAG = 1/1000;
 const FAST_DRAG = 1/5000;
 
-console.log('Creating run animation function');
-const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
-
-console.log('Creating routeFrame function');
-function routeFrame(mario) {
-    if (mario.jump.falling) {
-        return 'jump';
-    }
-
-    if (mario.go.distance > 0) {
-        if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
-            return 'break';
-        }
-
-        return runAnim(mario.go.distance);
-    }
-
-    return 'idle';
-}
-
 export function loadMario() {
     return loadSpriteSheet('mario')
     .then(createMarioFactory);
@@ -34,6 +14,22 @@ export function loadMario() {
 
 function createMarioFactory(sprite) {
     const runAnim = sprite.animations.get('run');
+
+    function routeFrame(mario) {
+        if (mario.jump.falling) {
+            return 'jump';
+        }
+
+        if (mario.go.distance > 0) {
+            if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
+                return 'break';
+            }
+
+            return runAnim(mario.go.distance);
+        }
+
+        return 'idle';
+    }
 
     function drawMario(context) {
         sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
