cd33c3edfef906974373dbaa089cd14ec2dbfe08
Pontus Persson <pontus.alexander@gmail.com>
2017-10-20 08:39:37 +0200

    Separate createMario function from loadMario Promise chain.

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -9,40 +9,42 @@ const FAST_DRAG = 1/5000;
 
 export function loadMario() {
     return loadSpriteSheet('mario')
-    .then(sprite => {
-        const mario = new Entity();
-        mario.size.set(14, 16);
+    .then(createMario);
+}
 
-        mario.addTrait(new Go());
-        mario.go.dragFactor = SLOW_DRAG;
+function createMario(sprite) {
+    const mario = new Entity();
+    mario.size.set(14, 16);
 
-        mario.addTrait(new Jump());
+    mario.addTrait(new Go());
+    mario.go.dragFactor = SLOW_DRAG;
 
-        mario.turbo = function setTurboState(turboOn) {
-            this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+    mario.addTrait(new Jump());
+
+    mario.turbo = function setTurboState(turboOn) {
+        this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+    }
+
+    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
+    function routeFrame(mario) {
+        if (mario.jump.falling) {
+            return 'jump';
         }
 
-        const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
-        function routeFrame(mario) {
-            if (mario.jump.falling) {
-                return 'jump';
+        if (mario.go.distance > 0) {
+            if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
+                return 'break';
             }
 
-            if (mario.go.distance > 0) {
-                if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
-                    return 'break';
-                }
-
-                return runAnim(mario.go.distance);
-            }
-
-            return 'idle';
+            return runAnim(mario.go.distance);
         }
 
-        mario.draw = function drawMario(context) {
-            sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-        }
+        return 'idle';
+    }
 
-        return mario;
-    });
+    mario.draw = function drawMario(context) {
+        sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
+    }
+
+    return mario;
 }
