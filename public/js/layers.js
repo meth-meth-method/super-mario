44a84e4448eee549cd44e5ada556467aea875a25
Pontus Persson <pontus.alexander@gmail.com>
2017-10-21 09:51:09 +0200

    Replace beginPath with strokeRect.

--- public/js/layers.js
+++ public/js/layers.js
@@ -70,23 +70,19 @@ export function createCollisionLayer(level) {
     return function drawCollision(context, camera) {
         context.strokeStyle = 'blue';
         resolvedTiles.forEach(({x, y}) => {
-            context.beginPath();
-            context.rect(
+            context.strokeRect(
                 x * tileSize - camera.pos.x,
                 y * tileSize - camera.pos.y,
                 tileSize, tileSize);
-            context.stroke();
         });
 
         context.strokeStyle = 'red';
         level.entities.forEach(entity => {
-            context.beginPath();
-            context.rect(
+            context.strokeRect(
                 entity.pos.x - camera.pos.x,
                 entity.pos.y - camera.pos.y,
                 entity.size.x,
                 entity.size.y);
-            context.stroke();
         });
 
         resolvedTiles.length = 0;
@@ -96,12 +92,10 @@ export function createCollisionLayer(level) {
 export function createCameraLayer(cameraToDraw) {
     return function drawCameraRect(context, fromCamera) {
         context.strokeStyle = 'purple';
-        context.beginPath();
-        context.rect(
+        context.strokeRect(
             cameraToDraw.pos.x - fromCamera.pos.x,
             cameraToDraw.pos.y - fromCamera.pos.y,
             cameraToDraw.size.x,
             cameraToDraw.size.y);
-        context.stroke();
     };
 }
