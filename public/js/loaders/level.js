c22659a7822d6b37d3b1c995686ee663d1664f46
Pontus Persson <pontus.alexander@gmail.com>
2017-10-21 11:52:29 +0200

    Separate tiles into layers.
    * Split tiles into layers in level JSON.
    * Flatten tiles layers for collision layer.
    * Parse all layers as separate drawing layers.

--- public/js/loaders/level.js
+++ public/js/loaders/level.js
@@ -95,12 +95,17 @@ export function loadLevel(name) {
     .then(([levelSpec, backgroundSprites]) => {
         const level = new Level();
 
-        const collisionGrid = createCollisionGrid(levelSpec.tiles, levelSpec.patterns);
+        const mergedTiles = levelSpec.layers.reduce((mergedTiles, layerSpec) => {
+            return mergedTiles.concat(layerSpec.tiles);
+        }, []);
+        const collisionGrid = createCollisionGrid(mergedTiles, levelSpec.patterns);
         level.setCollisionGrid(collisionGrid);
 
-        const backgroundGrid = createBackgroundGrid(levelSpec.tiles, levelSpec.patterns);
-        const backgroundLayer = createBackgroundLayer(level, backgroundGrid, backgroundSprites);
-        level.comp.layers.push(backgroundLayer);
+        levelSpec.layers.forEach(layerSpec => {
+            const backgroundGrid = createBackgroundGrid(layerSpec.tiles, levelSpec.patterns);
+            const backgroundLayer = createBackgroundLayer(level, backgroundGrid, backgroundSprites);
+            level.comp.layers.push(backgroundLayer);
+        });
 
         const spriteLayer = createSpriteLayer(level.entities);
         level.comp.layers.push(spriteLayer);
