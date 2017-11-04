73790beb9038a09bccf183f21fb41b4ba7cc67a0
Pontus Persson <pontus.alexander@gmail.com>
2017-11-04 13:06:07 +0100

    Make main use async/await.

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,14 @@ import {loadEntities} from './entities.js';
 import {setupKeyboard} from './input.js';
 import {createCollisionLayer} from './layers.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
+async function main(canvas) {
+    const context = canvas.getContext('2d');
 
-loadEntities()
-.then(entityFactory => Promise.all([
-    entityFactory,
-    createLevelLoader(entityFactory),
-]))
-.then(([entityFactory, loadLevel]) => Promise.all([
-    entityFactory,
-    loadLevel('1-1'),
-]))
-.then(([entityFactory, level]) => {
+    const entityFactory = await loadEntities();
+    const loadLevel = createLevelLoader(entityFactory);
+
+    const level = await loadLevel('1-1');
+
     const camera = new Camera();
 
     const mario = entityFactory.mario();
@@ -42,4 +37,7 @@ loadEntities()
     }
 
     timer.start();
-});
+}
+
+const canvas = document.getElementById('screen');
+main(canvas);
