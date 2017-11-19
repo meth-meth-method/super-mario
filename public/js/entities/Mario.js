9f2b2c5891c426aa8fc016a14ab6e4269cd30da6
Pontus Persson <pontus.alexander@gmail.com>
2017-11-19 19:27:21 +0100

    Add Killable trait to Mario.

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -2,6 +2,7 @@ import Entity from '../Entity.js';
 import Go from '../traits/Go.js';
 import Jump from '../traits/Jump.js';
 import Stomper from '../traits/Stomper.js';
+import Killable from '../traits/Killable.js';
 import {loadSpriteSheet} from '../loaders.js';
 
 const SLOW_DRAG = 1/1000;
@@ -46,6 +47,7 @@ function createMarioFactory(sprite) {
         mario.addTrait(new Go());
         mario.addTrait(new Jump());
         mario.addTrait(new Stomper());
+        mario.addTrait(new Killable());
 
         mario.turbo = setTurboState;
         mario.draw = drawMario;
