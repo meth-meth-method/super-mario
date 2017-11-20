a2fd9573bdc683fe1ba04ad5a76ee2fe6d93c8c2
Pontus Persson <pontus.alexander@gmail.com>
2017-11-20 17:23:41 +0100

    Copy Goomba behavior into Koopa.

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,10 +1,33 @@
-import Entity, {Sides} from '../Entity.js';
+import Entity, {Sides, Trait} from '../Entity.js';
+import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js';
 import {loadSpriteSheet} from '../loaders.js';
 
 export function loadKoopa() {
     return loadSpriteSheet('koopa')
     .then(createKoopaFactory);
+}
+
+class Behavior extends Trait {
+    constructor() {
+        super('behavior');
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                them.stomper.bounce(them, us);
+                us.killable.kill();
+                us.pendulumWalk.speed = 0;
+            } else {
+                them.killable.kill();
+            }
+        }
+    }
 }
 
 function createKoopaFactory(sprite) {
@@ -20,6 +43,8 @@ function createKoopaFactory(sprite) {
         koopa.offset.y = 8;
 
         koopa.addTrait(new PendulumWalk());
+        koopa.addTrait(new Killable());
+        koopa.addTrait(new Behavior());
 
         koopa.draw = drawKoopa;
 
