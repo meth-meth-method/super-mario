7c11ba82f1ae693e4642b79d754dc81f2c6b532c
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:41:01 +0100

    Use font size to derive line no.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,7 +1,9 @@
 export function createDashboardLayer(font) {
+    const LINE1 = font.size * 1;
+
     return function drawDashboardLayer(context) {
-        font.print("MARIO", context, 16, 8);
-        font.print("WORLD", context, 152, 8);
-        font.print("TIME", context, 208, 8);
+        font.print("MARIO", context, 16, LINE1);
+        font.print("WORLD", context, 152, LINE1);
+        font.print("TIME", context, 208, LINE1);
     };
 }
