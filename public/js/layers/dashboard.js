f25c36b33970cede78340faa0e9e3e1b128fd3ad
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:42:52 +0100

    Print score.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,9 +1,14 @@
 export function createDashboardLayer(font) {
-    const LINE1 = font.size * 1;
+    const LINE1 = font.size;
+    const LINE2 = font.size * 2;
+
+    const score = 1234;
 
     return function drawDashboardLayer(context) {
         font.print("MARIO", context, 16, LINE1);
         font.print("WORLD", context, 152, LINE1);
         font.print("TIME", context, 208, LINE1);
+
+        font.print(score.toString(), context, 16, LINE2);
     };
 }
