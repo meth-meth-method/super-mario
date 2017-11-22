8d43477aa54b6fd8ef332ed16b4bd132914afb29
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:38:40 +0100

    Print "WORLD".

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,6 +1,7 @@
 export function createDashboardLayer(font) {
     return function drawDashboardLayer(context) {
         font.print("MARIO", context, 16, 8);
+        font.print("WORLD", context, 152, 8);
         font.print("TIME", context, 208, 8);
     };
 }
