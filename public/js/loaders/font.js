b6f2016979ce6f4a7d6083851e9c966d323e7894
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:24:17 +0100

    Escape char index string.

--- public/js/loaders/font.js
+++ public/js/loaders/font.js
@@ -1,7 +1,7 @@
 import {loadImage} from '../loaders.js';
 import SpriteSheet from '../SpriteSheet.js';
 
-const CHARS =  !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~;
+const CHARS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
 
 export function loadFont() {
     return loadImage('./img/font.png')
