53af24e8a978cf833682ed00e5a7019039f1000e
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:25:41 +0100

    Spell out "MARIO".

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -2,6 +2,9 @@ export function createDashboardLayer(font) {
     return function drawDashboardLayer(context) {
         font.draw("M", context, 0, 0);
         font.draw("A", context, 8, 0);
+        font.draw("R", context, 16, 0);
+        font.draw("I", context, 24, 0);
+        font.draw("O", context, 32, 0);
 
     };
 }
