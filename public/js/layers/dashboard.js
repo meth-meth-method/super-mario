feb11244189a3b6d145499f4f21a096f7b33eee0
Pontus Persson <pontus.alexander@gmail.com>
2017-11-23 00:07:44 +0100

    Replace @ with coin, and x with thin x in font image and print coin status.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -8,16 +8,20 @@ export function createDashboardLayer(font, playerEnv) {
     const LINE2 = font.size * 2;
 
     const score = 1234;
+    const coins = 9;
 
     font.print("MARIO", bufferContext, 16, LINE1);
     font.print("WORLD", bufferContext, 152, LINE1);
     font.print("1-1", bufferContext, 160, LINE2);
     font.print("TIME", bufferContext, 208, LINE1);
 
+    font.print("@x", bufferContext, 96, LINE2);
+
     return function drawDashboardLayer(context) {
         const time = playerEnv.playerController.time.toFixed();
 
         font.print(score.toString().padStart(6, "0"), context, 16, LINE2);
+        font.print(coins.toString().padStart(2, "0"), context, 112, LINE2);
         font.print(time.toString().padStart(3, "0"), context, 216, LINE2);
 
         context.drawImage(buffer, 0, 0);
