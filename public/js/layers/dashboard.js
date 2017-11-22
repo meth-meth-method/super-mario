f64da4c4d1b477c1ba3603d92db183ec8568b479
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:25:11 +0100

    Spell "MA".

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,5 +1,7 @@
 export function createDashboardLayer(font) {
     return function drawDashboardLayer(context) {
-        font.draw("A", context, 0, 0);
+        font.draw("M", context, 0, 0);
+        font.draw("A", context, 8, 0);
+
     };
 }
