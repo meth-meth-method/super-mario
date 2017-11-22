fd1b59861fd1beb60701716b8ce6021ed1a34a3c
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:50:34 +0100

    Draw on buffer.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,17 +1,24 @@
 export function createDashboardLayer(font, playerEnv) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 32;
+    const bufferContext = buffer.getContext('2d');
+
     const LINE1 = font.size;
     const LINE2 = font.size * 2;
 
     const score = 1234;
 
+    font.print("MARIO", bufferContext, 16, LINE1);
+    font.print("WORLD", bufferContext, 152, LINE1);
+    font.print("TIME", bufferContext, 208, LINE1);
+
     return function drawDashboardLayer(context) {
         const time = playerEnv.playerController.time.toFixed();
 
-        font.print("MARIO", context, 16, LINE1);
-        font.print("WORLD", context, 152, LINE1);
-        font.print("TIME", context, 208, LINE1);
-
         font.print(score.toString().padStart(6, "0"), context, 16, LINE2);
         font.print(time.toString().padStart(3, "0"), context, 216, LINE2);
+
+        context.drawImage(buffer, 0, 0);
     };
 }
