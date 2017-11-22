5745ab3658d7014817031c12405556d649ba9436
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 21:26:46 +0100

    Decouple BoundingBox drawing function.

--- public/js/layers/collision.js
+++ public/js/layers/collision.js
@@ -1,3 +1,18 @@
+function createBoundingBoxLayer(entities) {
+    return function drawBoundingBoxes(context, camera) {
+        context.strokeStyle = 'red';
+        entities.forEach(entity => {
+            context.beginPath();
+            context.rect(
+                entity.bounds.left - camera.pos.x,
+                entity.bounds.top - camera.pos.y,
+                entity.size.x,
+                entity.size.y);
+            context.stroke();
+        });
+    }
+}
+
 export function createCollisionLayer(level) {
     const resolvedTiles = [];
 
@@ -10,6 +25,8 @@ export function createCollisionLayer(level) {
         return getByIndexOriginal.call(tileResolver, x, y);
     }
 
+    const drawBoundingBoxes = createBoundingBoxLayer(level.entities);
+
     return function drawCollision(context, camera) {
         context.strokeStyle = 'blue';
         resolvedTiles.forEach(({x, y}) => {
@@ -21,16 +38,7 @@ export function createCollisionLayer(level) {
             context.stroke();
         });
 
-        context.strokeStyle = 'red';
-        level.entities.forEach(entity => {
-            context.beginPath();
-            context.rect(
-                entity.bounds.left - camera.pos.x,
-                entity.bounds.top - camera.pos.y,
-                entity.size.x,
-                entity.size.y);
-            context.stroke();
-        });
+        drawBoundingBoxes(context, camera);
 
         resolvedTiles.length = 0;
     };
