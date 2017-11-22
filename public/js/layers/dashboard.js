aea08ec06c2560231aa1410a9d34dceb2e8e7029
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:31:01 +0100

    Spell Mario using Font class.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,10 +1,5 @@
 export function createDashboardLayer(font) {
     return function drawDashboardLayer(context) {
-        font.draw("M", context, 0, 0);
-        font.draw("A", context, 8, 0);
-        font.draw("R", context, 16, 0);
-        font.draw("I", context, 24, 0);
-        font.draw("O", context, 32, 0);
-
+        font.print("MARIO", context, 0, 0);
     };
 }
