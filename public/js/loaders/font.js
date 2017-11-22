4e3ef4497a1d0475b3b5f1bab3944345150e4a22
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:24:30 +0100

    Define all characters.

--- public/js/loaders/font.js
+++ public/js/loaders/font.js
@@ -8,7 +8,13 @@ export function loadFont() {
     .then(image => {
         const fontSprite = new SpriteSheet(image);
 
-        fontSprite.define("A", 8, 16, 8, 8);
+        const size = 8;
+        const rowLen = image.width;
+        for (let [index, char] of [...CHARS].entries()) {
+            const x = index * size % rowLen;
+            const y = Math.floor(index * size / rowLen) * size;
+            fontSprite.define(char, x, y, size, size);
+        }
 
         return fontSprite;
     });
