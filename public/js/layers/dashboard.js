91b6b95de6f4061c77d9b6c504e7c205faf479c3
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 23:44:13 +0100

    Print out Time.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -3,6 +3,7 @@ export function createDashboardLayer(font) {
     const LINE2 = font.size * 2;
 
     const score = 1234;
+    const time = 294;
 
     return function drawDashboardLayer(context) {
         font.print("MARIO", context, 16, LINE1);
@@ -10,5 +11,6 @@ export function createDashboardLayer(font) {
         font.print("TIME", context, 208, LINE1);
 
         font.print(score.toString().padStart(6, "0"), context, 16, LINE2);
+        font.print(time.toString().padStart(3, "0"), context, 216, LINE2);
     };
 }
