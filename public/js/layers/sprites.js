2d89c2da550ee53a2e5a85aa2e6cc1dcf11b8774
Pontus Persson <pontus.alexander@gmail.com>
2017-11-22 21:14:57 +0100

    Decouple sprite layer to module.

---
+++ public/js/layers/sprites.js
@@ -0,0 +1,19 @@
+export function createSpriteLayer(entities, width = 64, height = 64) {
+    const spriteBuffer = document.createElement('canvas');
+    spriteBuffer.width = width;
+    spriteBuffer.height = height;
+    const spriteBufferContext = spriteBuffer.getContext('2d');
+
+    return function drawSpriteLayer(context, camera) {
+        entities.forEach(entity => {
+            spriteBufferContext.clearRect(0, 0, width, height);
+
+            entity.draw(spriteBufferContext);
+
+            context.drawImage(
+                spriteBuffer,
+                entity.pos.x - camera.pos.x,
+                entity.pos.y - camera.pos.y);
+        });
+    };
+}
