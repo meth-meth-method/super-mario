fb63412ef61e778170c39044edeffed68fd19f40
Pontus Persson <pontus.alexander@gmail.com>
2017-11-25 09:16:19 +0100

    Remove canCollide flag.

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -28,8 +28,6 @@ export class Trait {
 
 export default class Entity {
     constructor() {
-        this.canCollide = true;
-
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
         this.size = new Vec2(0, 0);
