c888de6c09356bd2cc2284cf3982910f8d7a070c
Pontus Persson <pontus.alexander@gmail.com>
2017-11-25 09:12:12 +0100

    Move all obstruct logic into solid trait.

--- public/js/traits/Solid.js
+++ public/js/traits/Solid.js
@@ -9,6 +9,15 @@ export default class Solid extends Trait {
         if (side === Sides.BOTTOM) {
             entity.bounds.bottom = match.y1;
             entity.vel.y = 0;
+        } else if  (side === Sides.TOP) {
+            entity.bounds.top = match.y2;
+            entity.vel.y = 0;
+        } else if (side === Sides.LEFT) {
+            entity.bounds.left = match.x2;
+            entity.vel.x = 0;
+        } else if (side === Sides.RIGHT) {
+            entity.bounds.right = match.x1;
+            entity.vel.x = 0;
         }
     }
 }
