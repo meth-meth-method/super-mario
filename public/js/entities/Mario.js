81541028c8c83e77a8e0aeaf5097b3d84f09ac6c
Pontus Persson <pontus.alexander@gmail.com>
2017-11-25 09:21:39 +0100

    Add Physics trait to Mario.

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -2,6 +2,7 @@ import Entity from '../Entity.js';
 import Go from '../traits/Go.js';
 import Jump from '../traits/Jump.js';
 import Killable from '../traits/Killable.js';
+import Physics from '../traits/Physics.js';
 import Solid from '../traits/Solid.js';
 import Stomper from '../traits/Stomper.js';
 import {loadSpriteSheet} from '../loaders.js';
@@ -45,6 +46,7 @@ function createMarioFactory(sprite) {
         const mario = new Entity();
         mario.size.set(14, 16);
 
+        mario.addTrait(new Physics());
         mario.addTrait(new Go());
         mario.addTrait(new Jump());
         mario.addTrait(new Solid());
