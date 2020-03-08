8b707e56a4e4e42af7e7b5056a959845cb96d2fa
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-08 19:44:07 +0100

    Setup Cannon loader

--- public/js/entities.js
+++ public/js/entities.js
@@ -2,7 +2,7 @@ import {loadBulletBill} from './entities/BulletBill.js';
 import {loadMario} from './entities/Mario.js';
 import {loadGoomba} from './entities/Goomba.js';
 import {loadKoopa} from './entities/Koopa.js';
-
+import {loadCannon} from './entities/Cannon.js';
 
 export async function loadEntities(audioContext) {
     const entityFactories = {};
@@ -11,18 +11,14 @@ export async function loadEntities(audioContext) {
         return factory => entityFactories[name] = factory;
     }
 
-    return Promise.all([
+    await Promise.all([
         loadMario(audioContext).then(addAs('mario')),
         loadGoomba(audioContext).then(addAs('goomba')),
         loadKoopa(audioContext).then(addAs('koopa')),
         loadBulletBill(audioContext).then(addAs('bullet-bill')),
+    ]);
 
-    await Promise.all([
-        loadMario().then(addAs('mario')),
-        loadGoomba().then(addAs('goomba')),
-        loadKoopa().then(addAs('koopa')),
-        loadBulletBill().then(addAs('bullet-bill')),
-    ])
+    entityFactories['cannon'] = loadCannon(entityFactories);
 
     return entityFactories;
 }
