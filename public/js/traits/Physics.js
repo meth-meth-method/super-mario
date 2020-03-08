df58e337c8f8f8df351429774d0e95283af830ad
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-08 10:33:54 +0100

    Use Gravity trait in Physics

--- public/js/traits/Physics.js
+++ public/js/traits/Physics.js
@@ -1,17 +1,21 @@
 import {Trait} from '../Entity.js';
+import Gravity from './Gravity.js';
 
 export default class Physics extends Trait {
     constructor() {
         super('physics');
+        this.gravity = new Gravity();
     }
 
-    update(entity, {deltaTime}, level) {
+    update(entity, gameContext, level) {
+        const {deltaTime} = gameContext;
+
         entity.pos.x += entity.vel.x * deltaTime;
         level.tileCollider.checkX(entity);
 
         entity.pos.y += entity.vel.y * deltaTime;
         level.tileCollider.checkY(entity);
 
-        entity.vel.y += level.gravity * deltaTime;
+        this.gravity.update(entity, gameContext, level);
     }
 }
