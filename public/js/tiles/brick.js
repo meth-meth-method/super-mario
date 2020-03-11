a52a1f17d20e6f16e7dd7247526deef99abadafb
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-11 22:52:28 +0100

    Implement brick collider as ground

--- public/js/tiles/brick.js
+++ public/js/tiles/brick.js
@@ -1,5 +1,27 @@
+import {Sides} from '../Entity.js';
+
 function handleX(match, entity) {
-    console.log("Found brick", match);
+    if (entity.vel.x > 0) {
+        if (entity.bounds.right > match.x1) {
+            entity.obstruct(Sides.RIGHT, match);
+        }
+    } else if (entity.vel.x < 0) {
+        if (entity.bounds.left < match.x2) {
+            entity.obstruct(Sides.LEFT, match);
+        }
+    }
 }
 
-export const brick = [handleX, handleX];
+function handleY(match, entity, tiles) {
+    if (entity.vel.y > 0) {
+        if (entity.bounds.bottom > match.y1) {
+            entity.obstruct(Sides.BOTTOM, match);
+        }
+    } else if (entity.vel.y < 0) {
+        if (entity.bounds.top < match.y2) {
+            entity.obstruct(Sides.TOP, match);
+        }
+    }
+}
+
+export const brick = [handleX, handleY];
