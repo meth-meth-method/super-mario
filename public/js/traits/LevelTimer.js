ea4a47417fa326fab85a0d7b4324e41bb8eb01a1
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-22 19:41:48 +0100

    Add LevelTimer trait

---
+++ public/js/traits/LevelTimer.js
@@ -0,0 +1,26 @@
+import {Trait} from '../Entity.js';
+
+export default class LevelTimer extends Trait {
+    static EVENT_TIMER_HURRY = Symbol('timer hurry');
+    static EVENT_TIMER_OK = Symbol('timer ok');
+
+    constructor() {
+        super('levelTimer');
+        this.totalTime = 300;
+        this.currentTime = this.totalTime;
+        this.hurryTime = 295;
+        this.hurryEmitted = null;
+    }
+
+    update(entity, {deltaTime}, level) {
+        this.currentTime -= deltaTime * 2;
+        if (this.hurryEmitted !== true && this.currentTime < this.hurryTime) {
+            level.events.emit(LevelTimer.EVENT_TIMER_HURRY);
+            this.hurryEmitted = true;
+        }
+        if (this.hurryEmitted !== false && this.currentTime > this.hurryTime) {
+            level.events.emit(LevelTimer.EVENT_TIMER_OK);
+            this.hurryEmitted = false;
+        }
+    }
+}
