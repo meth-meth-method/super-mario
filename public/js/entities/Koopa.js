c381574cb23981bc32b59fd8e465bb91d87dda2d
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-23 16:03:24 +0100

    Fix Koopa update loop bug

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -89,7 +89,7 @@ class Behavior extends Trait {
         this.state = STATE_PANIC;
     }
 
-    update(us, deltaTime) {
+    update(us, {deltaTime}) {
         if (this.state === STATE_HIDING) {
             this.hideTime += deltaTime;
             if (this.hideTime > this.hideDuration) {
