65e5ad286d14282c83c5edf68298eb2ddde0138a
Pontus Alexander <pontus.alexander@gmail.com>
2020-03-24 23:12:35 +0100

    Use new API in Player trait

--- public/js/traits/Player.js
+++ public/js/traits/Player.js
@@ -1,9 +1,15 @@
 import {Trait} from '../Entity.js';
+import Stomper from '../traits/Stomper.js';
 
 export default class Player extends Trait {
     constructor() {
         super('player');
         this.lives = 3;
         this.score = 0;
+
+        this.listen(Stomper.EVENT_STOMP, () => {
+            this.score += 100;
+            console.log("Stomp", this.score);
+        });
     }
 }
