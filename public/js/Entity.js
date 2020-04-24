adbe055ad9575931efe8d7551a00f70519e14b81
Pontus Alexander <pontus.alexander@gmail.com>
2020-04-24 21:51:27 +0200

    Supply entity in queue task

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -87,7 +87,7 @@ export default class Entity {
     }
 
     finalize() {
-        this.events.emit(Trait.EVENT_TASK);
+        this.events.emit(Trait.EVENT_TASK, this);
 
         this.traits.forEach(trait => {
             trait.finalize(this);
