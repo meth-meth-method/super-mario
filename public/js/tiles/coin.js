2162befa82ea34f91362a0633833023360613c7f
Pontus Alexander <pontus.alexander@gmail.com>
2020-04-24 21:38:28 +0200

    Trigger addCoins method in Tile handler

--- public/js/tiles/coin.js
+++ public/js/tiles/coin.js
@@ -2,6 +2,7 @@ import {Sides} from '../Entity.js';
 
 function handle({entity, match, resolver, gameContext, level}) {
     if (entity.player) {
+        entity.player.addCoins(1);
         const grid = resolver.matrix;
         grid.delete(match.indexX, match.indexY);
     }
