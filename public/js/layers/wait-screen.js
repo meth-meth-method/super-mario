42b75b9bba015eeea435d2f2a2ae3a5344052346
Pontus Alexander <pontus.alexander@gmail.com>
2020-04-27 18:36:14 +0200

    Clear AvatarBuffer in WaitScreen

--- public/js/layers/wait-screen.js
+++ public/js/layers/wait-screen.js
@@ -7,20 +7,21 @@ function getPlayer(level) {
 }
 
 export function createWaitScreenRenderer(font) {
-    const buffer = document.createElement('canvas');
-    buffer.width = 32;
-    buffer.height = 32;
+    const avatarBuffer = document.createElement('canvas');
+    avatarBuffer.width = 32;
+    avatarBuffer.height = 32;
 
-    const avatarContext = buffer.getContext('2d');
+    const avatarContext = avatarBuffer.getContext('2d');
 
     return function createWaitScreenLayer(level) {
         const entity = getPlayer(level);
+        avatarContext.clearRect(0, 0, avatarBuffer.height, avatarBuffer.width);
         entity.draw(avatarContext);
 
         return function drawWaitScreen(context) {
             font.print('WORLD ' + level.name, context, 8 * 12, 8 * 12);
             font.print('x ' + entity.player.lives.toString().padStart(3, ' '), context, 8 * 16, 8 * 16);
-            context.drawImage(buffer, 8 * 12, 8 * 15);
+            context.drawImage(avatarBuffer, 8 * 12, 8 * 15);
         };
     };
 };
