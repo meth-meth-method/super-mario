85c7bc3a58825e33d2a5e17208fd52fd5bf9f9b7
Pontus Alexander <pontus.alexander@gmail.com>
2020-04-27 18:27:08 +0200

    Add SceneRunner class

---
+++ public/js/SceneRunner.js
@@ -0,0 +1,31 @@
+import Scene from "./Scene.js";
+
+export default class SceneRunner {
+    constructor() {
+        this.sceneIndex = -1;
+        this.scenes = [];
+    }
+
+    addScene(scene) {
+        scene.events.listen(Scene.EVENT_COMPLETE, () => {
+            this.runNext();
+        });
+        this.scenes.push(scene);
+    }
+
+    runNext() {
+        const currentScene = this.scenes[this.sceneIndex];
+        if (currentScene) {
+            currentScene.end();
+        }
+        this.sceneIndex++;
+    }
+
+    update(gameContext) {
+        const currentScene = this.scenes[this.sceneIndex];
+        if (!currentScene) {
+            return;
+        }
+        currentScene.update(gameContext);
+    }
+}
