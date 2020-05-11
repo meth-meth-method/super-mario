f7218d29ebd65c43e57bdc44580ab2e9ae096c8e
Johan Bouveng <johan.bouveng@gmail.com>
2020-05-11 19:36:02 +0200

    Correct positioning of dashboard and values.

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -17,23 +17,23 @@ function getTimerTrait(entities) {
 }
 
 export function createDashboardLayer(font, level) {
-    const LINE1 = font.size;
-    const LINE2 = font.size * 2;
+    const LINE1 = font.size * 2;
+    const LINE2 = font.size * 3;
 
     const timerTrait = getTimerTrait(level.entities);
 
     return function drawDashboard(context) {
         const playerTrait = getPlayerTrait(level.entities);
 
-        font.print(playerTrait.name, context, 16, LINE1);
-        font.print(playerTrait.score.toString().padStart(6, '0'), context, 16, LINE2);
+        font.print(playerTrait.name, context, 24, LINE1);
+        font.print(playerTrait.score.toString().padStart(6, '0'), context, 24, LINE2);
 
-        font.print('x' + playerTrait.coins.toString().padStart(2, '0'), context, 96, LINE2);
+        font.print('x' + playerTrait.coins.toString().padStart(2, '0'), context, 88, LINE2);
 
-        font.print('WORLD', context, 152, LINE1);
-        font.print(level.name, context, 160, LINE2);
+        font.print('WORLD', context, 136, LINE1);
+        font.print(level.name, context, 144, LINE2);
 
-        font.print('TIME', context, 208, LINE1);
-        font.print(timerTrait.currentTime.toFixed().toString().padStart(3, '0'), context, 216, LINE2);
+        font.print('TIME', context, 200, LINE1);
+        font.print(timerTrait.currentTime.toFixed().toString().padStart(3, '0'), context, 208, LINE2);
     };
 }
