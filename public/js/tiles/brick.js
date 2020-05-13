61e338bc4e44c92aca30d870a170f49dfd29ad2c
Pontus Alexander <pontus.alexander@gmail.com>
2020-05-13 10:37:10 +0200

    Emit sharpnel from bricks

--- public/js/tiles/brick.js
+++ public/js/tiles/brick.js
@@ -1,5 +1,28 @@
+import { Vec2 } from '../math.js';
 import {Sides} from '../Entity.js';
 import Player from "../traits/Player.js";
+
+function createSharpnel(gameContext, match) {
+    const center = new Vec2(
+        match.x1 + ((match.x2 - match.x1) / 2),
+        match.y1 + ((match.y2 - match.y1) / 2),
+    );
+
+    const bricks = [
+        gameContext.entityFactory.brickSharpnel(),
+        gameContext.entityFactory.brickSharpnel(),
+        gameContext.entityFactory.brickSharpnel(),
+        gameContext.entityFactory.brickSharpnel(),
+    ];
+    for (const brick of bricks) {
+        brick.pos.copy(center);
+    }
+    bricks[0].vel.set(-50, -400);
+    bricks[1].vel.set(-25, -400);
+    bricks[2].vel.set(25, -400);
+    bricks[3].vel.set(50, -400);
+    return bricks;
+}
 
 function handleX({entity, match}) {
 
@@ -24,10 +47,10 @@ function handleY({entity, match, resolver, gameContext, level}) {
             const grid = resolver.matrix;
             grid.delete(match.indexX, match.indexY);
 
-            const goomba = gameContext.entityFactory.goomba();
-            goomba.vel.set(50, -400);
-            goomba.pos.set(entity.pos.x, match.y1);
-            level.entities.add(goomba);
+            const shrapnel = createSharpnel(gameContext, match);
+            for (const entity of shrapnel) {
+                level.entities.add(entity);
+            }
         }
 
         if (entity.bounds.top < match.y2) {
