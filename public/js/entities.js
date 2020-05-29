c93bc39d7aeba834b42510bf29cfcf68fe51b733
Johan Bouveng <johan.bouveng@gmail.com>
2020-05-29 11:05:31 +0200

    Brown and blue Goombas. Green and blue Koopas.

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,6 +1,6 @@
 import {loadMario} from './entities/Mario.js';
-import {loadGoomba} from './entities/Goomba.js';
-import {loadKoopa} from './entities/Koopa.js';
+import {loadGoombaBrown, loadGoombaBlue} from './entities/Goomba.js';
+import {loadKoopaGreen, loadKoopaBlue} from './entities/Koopa.js';
 import {loadBullet} from './entities/Bullet.js';
 import {loadCannon} from './entities/Cannon.js';
 
@@ -13,8 +13,10 @@ export function loadEntities(audioContext) {
 
     return Promise.all([
         loadMario(audioContext).then(addAs('mario')),
-        loadGoomba(audioContext).then(addAs('goomba')),
-        loadKoopa(audioContext).then(addAs('koopa')),
+        loadGoombaBrown(audioContext).then(addAs('goomba-brown')),
+        loadGoombaBlue(audioContext).then(addAs('goomba-blue')),
+        loadKoopaGreen(audioContext).then(addAs('koopa-green')),
+        loadKoopaBlue(audioContext).then(addAs('koopa-blue')),
         loadBullet(audioContext).then(addAs('bullet')),
         loadCannon(audioContext).then(addAs('cannon')),
 
