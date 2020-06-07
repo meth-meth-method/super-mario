2ddf0fb624455a7e3d24bb12b8acb890b7fb277b
Pontus Alexander <pontus.alexander@gmail.com>
2020-06-07 13:27:15 +0200

    Remove PlayerEnv/Controller

--- public/js/player.js
+++ public/js/player.js
@@ -1,15 +1,4 @@
-import Entity from './Entity.js';
 import Player from './traits/Player.js';
-import PlayerController from './traits/PlayerController.js';
-
-export function createPlayerEnv(playerEntity) {
-    const playerEnv = new Entity();
-    const playerControl = new PlayerController();
-    playerControl.checkpoint.set(64, 64);
-    playerControl.setPlayer(playerEntity);
-    playerEnv.addTrait(playerControl);
-    return playerEnv;
-}
 
 export function makePlayer(entity, name) {
     const player = new Player();
